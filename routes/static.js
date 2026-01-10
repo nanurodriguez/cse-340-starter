50fbc2040444c36b86ad1393505316f68ccc6d20
nanurodriguez <nancy95rod@gmail.com>
2026-01-10 01:20:17 -0700

    adding final touches to the javascript so the server knows what to show to the user

--- routes/static.js
+++ routes/static.js
@@ -3,10 +3,20 @@ const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
+/*router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
+*/
+//Tell the server WHAT to show on the home page:
+
+// This tells Express to look for styles/images in the root "public" folder
+router.use(express.static("public"));
+
+// This defines the Home route
+router.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
 
 module.exports = router;
 
